2b69aa3493151ce963bd67ceef6ad47d4bb3d508
Aaron Griselda <165138671+wobbleface@users.noreply.github.com>
2024-03-27 09:56:37 +0200

    Create main.js

---
+++ main.js
@@ -0,0 +1,31 @@
+(function (exports) {
+  'use strict';
+
+  exports.LZW = function () {
+    this.dictionarySize = 256;
+  };
+
+  exports.LZW.compress = function (data) {
+    var i;
+    var dictionary = {};
+    var character;
+    var wc;
+    var w = '';
+    var result = [];
+
+    for (i = 0; i < this.dictionarySize; i = i + 1) {
+      dictionary[String.fromCharCode(i)] = i;
+    }
+
+    for (i = 0; i < data.length; i = i + 1) {
+      character = data.charAt(i);
+      wc = w + character;
+      if (dictionary.hasOwnProperty(wc)) {
+        w = wc;
+      } else {
+        result.push(dictionary[w]);
+        dictionary[wc] = this.dictionarySize;
+        this.dictionarySize = this.dictionarySize + 1;
+        w = String(character);
+      }
+    }
